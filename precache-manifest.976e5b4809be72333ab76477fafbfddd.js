62efa08a5a422b3daa4b3fe363bb1f349b239912
danial1021 <41097199+danial1021@users.noreply.github.com>
2020-06-06 22:24:51 +0900

    Updates

---
+++ precache-manifest.976e5b4809be72333ab76477fafbfddd.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a303244359f3b4e002d5cbcda2b21ac1",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "dc28a89ad37a63ec2230",
+    "url": "/portfolio/static/css/main.cc7ae59f.chunk.css"
+  },
+  {
+    "revision": "cb9cf4e32e4bac5daba6",
+    "url": "/portfolio/static/js/2.3c1962d4.chunk.js"
+  },
+  {
+    "revision": "dc28a89ad37a63ec2230",
+    "url": "/portfolio/static/js/main.71629e37.chunk.js"
+  },
+  {
+    "revision": "b0a63269e329c2954a2c",
+    "url": "/portfolio/static/js/runtime-main.c26ebb5f.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "e47c3e26372c670c4a4eeac6eea0bf98",
+    "url": "/portfolio/static/media/airbnbLogo.e47c3e26.jpg"
+  },
+  {
+    "revision": "d6d4086fb1cade58b3f786620e17c3ef",
+    "url": "/portfolio/static/media/contactMail.d6d4086f.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/portfolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/portfolio/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/portfolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/portfolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "0288c5ec7a87ea73f8d14b12fc4bc94e",
+    "url": "/portfolio/static/media/talksCardBack.0288c5ec.svg"
+  }
+]);
